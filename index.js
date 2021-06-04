d334e47fac46338abf4e18f5f736c80b26cddd49
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2021-06-04 14:43:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const balloon = document.getElementById('air-balloon')
+const cloudOne = document.getElementById('cloud-one')
+const cloudTwo = document.getElementById('cloud-two')
+const cloudThree = document.getElementById('cloud-three')
+const cloudFour = document.getElementById('cloud-four')
+const cloudFive = document.getElementById('cloud-five')
+const javascript = document.getElementById('javascript')
+const react = document.getElementById('react')
+const graphql = document.getElementById('graphql')
+
+
+function move() {
+  const incrementer = window.scrollY
+
+  // the '10' refers to the height from the bottom of the sky section. We added this
+  // in the css file. Feel free to move all styling to the JS file if you prefer to keep the
+  // the '10' consistent.
+  balloon.style.bottom = 10 + incrementer * 0.1 + '%'
+
+  cloudOne.style.bottom = 40 + incrementer * 0.12 + '%'
+  cloudOne.style.marginLeft = 70 + incrementer * 0.1 + '%'
+  cloudTwo.style.bottom = 80 + incrementer * 0.14 + '%'
+  cloudTwo.style.marginLeft = 60 + incrementer * 0.15 + '%'
+  cloudThree.style.bottom = 60 + incrementer * 0.1 + '%'
+  cloudThree.style.marginRight = 80 + incrementer * 0.12 + '%'
+  cloudFour.style.bottom = 70 + incrementer * 0.16 + '%'
+  cloudFour.style.marginRight = 40 + incrementer * 0.15 + '%'
+  cloudFive.style.bottom = 60 + incrementer * 0.2 + '%'
+  cloudFive.style.marginLeft = 40 + incrementer * 0.16 + '%'
+  javascript.style.marginRight = 55 + incrementer * 2.2 + '%'
+  react.style.marginRight = 43 + incrementer * 1.5 + '%'
+  graphql.style.marginRight = 31 + incrementer * 1 + '%'
+}
+
+window.addEventListener('scroll', move)
