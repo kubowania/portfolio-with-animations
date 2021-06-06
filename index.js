a63ab4b7c837103a27ff75869d1f220aaac6d6ea
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2021-06-06 17:54:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ function move() {
   balloon.style.bottom = 10 + incrementer * 0.1 + '%'
 
   cloudOne.style.bottom = 40 + incrementer * 0.12 + '%'
-  cloudOne.style.marginLeft = 70 + incrementer * 0.1 + '%'
+  cloudOne.style.left = 75 + incrementer * 0.1 + '%'
   cloudTwo.style.bottom = 80 + incrementer * 0.14 + '%'
-  cloudTwo.style.marginLeft = 60 + incrementer * 0.15 + '%'
+  cloudTwo.style.left = 70 + incrementer * 0.15 + '%'
   cloudThree.style.bottom = 60 + incrementer * 0.1 + '%'
-  cloudThree.style.marginRight = 80 + incrementer * 0.12 + '%'
+  cloudThree.style.left = 0 + incrementer * -0.12 + '%'
   cloudFour.style.bottom = 70 + incrementer * 0.16 + '%'
-  cloudFour.style.marginRight = 40 + incrementer * 0.15 + '%'
+  cloudFour.style.left = 20 + incrementer * -0.15 + '%'
   cloudFive.style.bottom = 60 + incrementer * 0.2 + '%'
-  cloudFive.style.marginLeft = 40 + incrementer * 0.16 + '%'
-  javascript.style.marginRight = 55 + incrementer * 2.2 + '%'
-  react.style.marginRight = 43 + incrementer * 1.5 + '%'
-  graphql.style.marginRight = 31 + incrementer * 1 + '%'
+  cloudFive.style.left = 60 + incrementer * 0.16 + '%'
+  javascript.style.left = 20 + incrementer * -2.2 + '%'
+  react.style.left = 26 + incrementer * -1.5 + '%'
+  graphql.style.left = 32 + incrementer * -1 + '%'
 }
 
 window.addEventListener('scroll', move)
